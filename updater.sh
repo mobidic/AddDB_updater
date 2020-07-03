92dd79ebb21e4293acec3b465ca9452855200d13
Gui~th <toma.guignard@laposte.net>
2020-07-03 19:12:26 +0200

    Add clinvar for annovar update

--- updater.sh
+++ updater.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
  echo "Missing argument: you should give your personal URL to download OMIM genemap2 file, provided from https://www.omim.org/downloads/  \n"
- echo "usage: updater.sh  https://data.omim.org/downloads/my-Registration-Code/genemap2.txt  \n"
+ echo "usage: sh updater.sh  https://data.omim.org/downloads/my-Registration-Code/genemap2.txt  [OPTIONAL]/path/to/genome/hg19_nochr.fasta   [OPTIOAL]/path/to/genome/hg38_nochr.fasta \n"
  exit 1
 fi
 
 
 
 # AddDB Updater
+
+#############################################
+#update gene_customfullxref for anovar
+echo #update gene_customfullxref for anovar
+#############################################
 
 ## Check if data folder exists and remove previous file
 
@@ -40,4 +45,95 @@ wget -P data $1
 
 ## Merge data into 
 python3 merge_db.py data/hgnc.tsv data/genemap2.txt data/gnomad.v2.1.1.lof_metrics.by_gene.txt data/uniprot.tsv
+
+
+#test ARG 2 : path to hg19 fasta
+if [ -n "$2" ]; then
+
+
+#############################################
+#update clinvar for anovar
+echo #update clinvar for anovar
+#############################################
+
+################ hg19 flavour ###############
+echo ########### hg19 flavour ###############
+
+#get last clinvar vcf
+wget -O data/hg19_clinvar.vcf.gz  ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz
+
+#extract file date 
+clindate=`zgrep -m 1 -P "##fileDate=2"  data/hg19_clinvar.vcf.gz | sed -e 's/##fileDate=//' -e 's/-//g'`
+
+#step1 split
+bcftools norm -m-both -o data/temp_hg19_clinvar_${clindate}_split.vcf data/hg19_clinvar.vcf.gz
  
+#preprocess splitted vcf
+./prepare_annovar_user.pl -dbtype clinvar_preprocess2   data/temp_hg19_clinvar_${clindate}_split.vcf  -out data/temp_hg19_clinvar_${clindate}_preproc.vcf
+
+#step2 left align preprocessed vcf
+bcftools norm -f $2 -o data/temp_hg19_clinvar_${clindate}_leftAlign.vcf  data/temp_hg19_clinvar_${clindate}_preproc.vcf
+ 
+ 
+#convert to annovar format
+./prepare_annovar_user.pl -dbtype clinvar2  data/temp_hg19_clinvar_${clindate}_leftAlign.vcf  -out data/hg19_clinvar_${clindate}.txt
+
+# index file by 1000 bin
+./index_annovar.pl   data/hg19_clinvar_${clindate}.txt  1000
+
+# Add header
+sed -i '1i #Chr\tStart\tEnd\tRef\tAlt\tCLNALLELEID\tCLNDN\tCLNDISDB\tCLNREVSTAT\tCLNSIG' data/hg19_clinvar_${clindate}.txt
+
+fi
+
+
+#test ARG 3 : path to hg38 fasta
+
+if [ -n "$3" ]; then
+
+############################################# 
+################ hg38 flavour ###############
+echo ########### hg38 flavour ###############
+
+#get last clinvar vcf
+wget -O data/hg38_clinvar.vcf.gz  ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz
+
+#get index script
+#git clone https://github.com/tsy19900929/cosmic2annovar.git
+
+#extract file date 
+clindate=`zgrep -m 1 -P "##fileDate=2"  data/hg38_clinvar.vcf.gz | sed -e 's/##fileDate=//' -e 's/-//g'`
+
+#step1 split
+bcftools norm -m-both -o data/temp_hg38_clinvar_${clindate}_split.vcf data/hg38_clinvar.vcf.gz
+ 
+#preprocess splitted vcf
+./prepare_annovar_user.pl -dbtype clinvar_preprocess2   data/temp_hg38_clinvar_${clindate}_split.vcf  -out data/temp_hg38_clinvar_${clindate}_preproc.vcf
+
+#step2 left align preprocessed vcf
+bcftools norm -f $3 -o data/temp_hg38_clinvar_${clindate}_leftAlign.vcf  data/temp_hg38_clinvar_${clindate}_preproc.vcf
+ 
+ 
+#convert to annovar format
+./prepare_annovar_user.pl -dbtype clinvar2  data/temp_hg38_clinvar_${clindate}_leftAlign.vcf  -out data/hg38_clinvar_${clindate}.txt
+ 
+# index file by 1000 bin
+./index_annovar.pl   data/hg38_clinvar_${clindate}.txt  1000
+
+# Add header
+sed -i '1i #Chr\tStart\tEnd\tRef\tAlt\tCLNALLELEID\tCLNDN\tCLNDISDB\tCLNREVSTAT\tCLNSIG' data/hg38_clinvar_${clindate}.txt
+
+
+
+fi
+
+
+# compress temp files
+gzip temp*
+
+
+exit 0
+
+
+
+
